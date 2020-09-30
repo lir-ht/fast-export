1b8f02b66be78bf6846d21b28d245c7b13d3e5fd
Henrik Tunedal <henrik.tunedal@leftisright.se>
2020-09-30 22:01:03 +0200

    Refactor test

--- tests/test_drop_plugin.py
+++ tests/test_drop_plugin.py
@@ -7,28 +7,44 @@ from pathlib import Path
 class CommitDropTest(TestCase):
     def setUp(self):
         self.tempdir = TemporaryDirectory()
+        self.hg = HgDriver(self.tempdir.name)
 
     def tearDown(self):
         self.tempdir.cleanup()
 
     def run_hg(self, *args):
-        p = subprocess.run(("hg", "-yq") + args,
-                           cwd=self.tempdir.name,
+        return self.hg.run_command(*args)
+
+    def write_file_data(self, data, filename='test_file.txt'):
+        path = Path(self.tempdir.name) / filename
+        with path.open('w') as f:
+            print(data, file=f)
+
+    def test_drop_single_commit(self):
+        self.write_file_data('line 1')
+        self.hg.init()
+        self.hg.commit('commit 1')
+
+        self.assertEqual('0 commit 1', self.hg.log())
+
+
+class HgDriver:
+    def __init__(self, repodir):
+        self.repodir = Path(repodir)
+
+    def run_command(self, *args):
+        p = subprocess.run(('hg', '-yq') + args,
+                           cwd=str(self.repodir),
                            check=True,
                            text=True,
                            capture_output=True)
         return p.stdout
 
-    def write_file(self, name, data):
-        path = Path(self.tempdir.name) / name
-        with path.open('w') as f:
-            print(data, file=f)
+    def init(self):
+        self.run_command('init')
 
-    def test_drop_single_commit(self):
-        self.run_hg("init")
-        self.write_file("test_file.txt", "line 1")
-        self.run_hg("commit", "-A", "-m", "commit 1")
+    def commit(self, message):
+        self.run_command('commit', '-A', '-m', message)
 
-        log = self.run_hg("log", "-T", "{rev} {desc}")
-        self.assertEqual("0 commit 1", log)
-
+    def log(self):
+        return self.run_command('log', '-T', '{rev} {desc}')
