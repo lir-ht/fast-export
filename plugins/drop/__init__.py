65f12b5031b10279abc69150daa4d163ee699b82
Henrik Tunedal <henrik.tunedal@leftisright.se>
2020-09-30 17:51:31 +0200

    Assume parents come before children

--- plugins/drop/__init__.py
+++ plugins/drop/__init__.py
@@ -15,7 +15,6 @@ class Filter:
         self.unresolved_hashes = {arg.encode('ascii', 'strict')
                                   for arg in args.split(',')}
         self.remapped_parents = {}
-        self.processed_parents = set()
 
     def commit_message_filter(self, commit_data):
         hg_hash = commit_data['hg_hash']
@@ -23,13 +22,9 @@ class Filter:
         parent_revs = commit_data['parents']
 
         if hg_hash in self.unresolved_hashes:
-            if rev in self.processed_parents:
-                raise Exception('Processed %i before resolving hash' % rev)
             self.remapped_parents[rev] = parent_revs
             self.unresolved_hashes.remove(hg_hash)
             log('Resolved hash to drop: %s -> %i', hg_hash, rev)
-
-        self.processed_parents.update(parent_revs)
 
         mapping = self.remapped_parents
         parent_revs = [rp for p in parent_revs for rp in mapping.get(p, [p])]
